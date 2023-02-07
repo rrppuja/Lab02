1e9d17d15398392353a670aa0a2cbdd7708ebead
rrppuja <rrppuja@myseneca.ca>
2023-02-06 21:59:21 -0500

    updated commit

--- server.js
+++ server.js
@@ -2,28 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("./public/"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "form.html"));
+  res.sendFile(path.join(__dirname, "views", "forms.html"));
 });
 
-app.post("/", (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-  const image = req.body.image;
-
-  res.render("thanks.css", {
-    firstName,
-    lastName,
-    email,
-    image
-  });
-});
-
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+app.listen(HTTP_PORT, onHttpStart);
