98547caee8b661e6a6095b38b1a3b48cffe80114
rrppuja <rrppuja@myseneca.ca>
2023-02-06 22:30:40 -0500

    updated commit

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
+const multer = require("multer");
 const HTTP_PORT = process.env.PORT || 8080;
 
-// call this function after the http server starts listening for requests
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
-}
+// multer requires a few options to be setup to store files with file extensions
+// by default it won't store extensions for security reasons
+const storage = multer.diskStorage({
+  destination: "./public/photos/",
+  filename: function (req, file, cb) {
+    // we write the filename as the current date down to the millisecond
+    // in a large web service this would possibly cause a problem if two people
+    // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
+    // this is a simple example.
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// tell multer to use the diskStorage function for naming files instead of the default.
+const upload = multer({ storage: storage });
+
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public/"));
 
 app.get("/", (req, res) => {
@@ -17,9 +29,13 @@ app.get("/", (req, res) => {
   
 });
 
-app.post("/", (req, res) => {
+app.post("/", upload.single("photo"), (req, res) => {
   res.send("Thank you for your submission");
 });
 
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
 
 app.listen(HTTP_PORT, onHttpStart);
